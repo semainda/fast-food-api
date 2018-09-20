a21e0d9869b4e7108a068d5778012f835203fe6a
semainda <semaindas@gmail.com>
2018-09-20 16:09:17 +0300

    [#160534977] Added test case for getting all orders

--- app/tests/v1/test_orders.py
+++ app/tests/v1/test_orders.py
@@ -1,7 +1,8 @@
 """This module contains ALL testcases for api endpoints in views.py module"""
-
+# Thirdparty imports
 import json
 import pytest
+
 
 @pytest.mark.parametrize(
     "new_order, status_code", [
@@ -11,8 +12,20 @@ import pytest
         (dict(item="Chicken", description="Saved with soup", quantity=2), 201)
         ]
     )
-
 def test_create_order_with_valid_entries(test_client, new_order, status_code):
-    """This function tests for creating order with all posibilities to return appropriate status code"""
+    """Test for creating order with all posibilities
+    to return appropriate status code"""
     response = test_client.post("/api/v1/orders", data=json.dumps(new_order))
     assert response.status_code == status_code
+
+
+def test_request_orders_which_does_not_exist(test_client):
+    """Tests for requesting orders which does not exist to return 404"""
+    response = test_client.get("/api/v1/orders")
+    assert response.status_code == 404
+
+
+def test_request_orders_which_does_exist(test_client):
+    """Test for requesting orders which does not exist to return 404"""
+    response = test_client.get("/api/v1/orders")
+    assert response.status_code == 200
