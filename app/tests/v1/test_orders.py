9a279bb1e53ddaa5a2850bba832e609bb62a2ba0
semainda <semaindas@gmail.com>
2018-09-20 23:50:15 +0300

    [#160534980] Added testcase for update specific order

--- app/tests/v1/test_orders.py
+++ app/tests/v1/test_orders.py
@@ -12,7 +12,7 @@ import pytest
         (dict(item="Chicken", description="Saved with soup", quantity=2), 201)
         ]
     )
-def test_create_order_with_valid_entries(test_client, new_order, status_code):
+def test_create_order(test_client, new_order, status_code):
     """Test for creating order with all posibilities
     to return appropriate status code"""
     response = test_client.post("/api/v1/orders", data=json.dumps(new_order))
@@ -42,3 +42,18 @@ def test_request_valid_order(test_client):
     """Test for requesting valid order to  return 200"""
     response = test_client.get("/api/v1/orders/1")
     assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "order_id, order_status, status_code", [
+        (3000, dict(), 404),
+        (1, dict(), 400),
+        (1, dict(status=1), 200)
+        ]
+    )
+def test_update_order(test_client, order_id, order_status, status_code):
+    """Test for updating order with four posibilities
+    to return appropriate status code"""
+    response = test_client.put(
+        "/api/v1/orders/order_id", data=json.dumps(order_status))
+    assert response.status_code == status_code
