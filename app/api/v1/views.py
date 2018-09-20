c6420d87ac1a19afa59ca1ffa214f0956c5d19a0
semainda <semaindas@gmail.com>
2018-09-20 14:17:21 +0300

    [#160534973] Added order creation view to pass the test

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -17,4 +17,26 @@ class Orders(Resource):
 
     def post(self):
         """Method for order creation"""
-        pass
+        json_data = request.get_json(force=True, silent=True)
+
+        if json_data:
+            if OrderItemsValodator(json_data['item']).text_validator():
+                if OrderItemsValodator(
+                        json_data['description']).text_validator():
+                    # if OrderItemsValodator(
+                    # json_data['quantity']).number_validator():
+                        ORDERS_DATA.append(
+                            dict(
+                                id=len(ORDERS_DATA) + 1,
+                                item=json_data["item"],
+                                description=json_data["description"],
+                                quantity=json_data["quantity"],
+                                order_date=DATE,
+                                status=STATUS_CODE
+                            )
+                        )
+                        return {"Message": "Order created successful"}, 201
+                    # return {"Message": "Invalid order quantity"}, 400
+                return {"Message": "Invalid order description"}, 400
+            return {"Message": "Invalid order item"}, 400
+        return {'Message': "You can not create empty order"}, 200
