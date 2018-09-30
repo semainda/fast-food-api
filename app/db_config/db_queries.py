25817a55c0bb09f924ac3987307d385fcbb378fc
semainda <semaindas@gmail.com>
2018-09-30 09:32:07 +0300

    ft #160787273 Update db_queries.py file with singular table names

--- app/db_config/db_queries.py
+++ app/db_config/db_queries.py
@@ -1,7 +1,7 @@
 """This module contains SQL Queries for both CREATE and DROP database tables for API_V2"""
 # Create database queries
 
-users = """CREATE TABLE IF NOT EXISTS users(
+users = """CREATE TABLE IF NOT EXISTS user(
                     user_id serial PRIMARY KEY,
                     first_name varchar(30) NOT NULL,
                     last_name varchar(30) NOT NULL,
@@ -12,27 +12,27 @@ users = """CREATE TABLE IF NOT EXISTS users(
                     password varchar(300) NOT NULL,
                     created_date date NOT NULL);"""
 
-categories = """CREATE TABLE IF NOT EXISTS categories(
+categories = """CREATE TABLE IF NOT EXISTS category(
                     cat_id serial PRIMARY KEY,
                     cat_name varchar(50) UNIQUE NOT NULL);"""
 
-meals = """CREATE TABLE IF NOT EXISTS meals(
+meals = """CREATE TABLE IF NOT EXISTS meal(
                     meal_id serial PRIMARY KEY,
                     meal_name varchar(50) NOT NULL,
-                    cat_id int REFERENCES categories(cat_id),
+                    cat_id int REFERENCES category(cat_id),
                     description varchar(80) NOT NULL,
                     price  int  NOT NULL);"""
 
-orders = """CREATE TABLE IF NOT EXISTS orders(
+orders = """CREATE TABLE IF NOT EXISTS order(
                     order_id serial PRIMARY KEY,
-                    user_id int REFERENCES users(user_id) ON DELETE CASCADE,
+                    user_id int REFERENCES user(user_id) ON DELETE CASCADE,
                     status int DEFAULT 0,
                     delivery_address text,
                     created_date date NOT NULL);"""
 
 orders_items = """CREATE TABLE IF NOT EXISTS orders_items(
-                    order_id int REFERENCES orders(order_id) ON DELETE CASCADE,
-                    meal_id int  REFERENCES meals(meal_id) ON DELETE RESTRICT,
+                    order_id int REFERENCES order(order_id) ON DELETE CASCADE,
+                    meal_id int  REFERENCES meal(meal_id) ON DELETE RESTRICT,
                     quantity int NOT NULL,
                     description  varchar(80) NOT NULL,
                     PRIMARY KEY(order_id, meal_id));"""
@@ -42,10 +42,10 @@ create_table_queries = [users, categories, meals, orders, orders_items]
 
 # Drop database queries
 
-users ="DROP TABLE IF EXISTS users CASCADE;"
-categories ="DROP TABLE IF EXISTS categories CASCADE;"
-meals = "DROP TABLE IF EXISTS meals CASCADE;"
-orders ="DROP TABLE IF EXISTS orders CASCADE;"
+users ="DROP TABLE IF EXISTS user CASCADE;"
+categories ="DROP TABLE IF EXISTS category CASCADE;"
+meals = "DROP TABLE IF EXISTS meal CASCADE;"
+orders ="DROP TABLE IF EXISTS order CASCADE;"
 orders_items ="DROP TABLE IF EXISTS orders_items CASCADE;"
 
 drop_table_queries = [users, categories, meals, orders, orders_items]
