891307f574495a4c3cacdbb35b951dc851a74b3a
semainda <semaindas@gmail.com>
2018-09-30 21:36:23 +0300

    ft #160784479 Add test for GET endpoints

--- app/tests/v2/orders/test_meal.py
+++ app/tests/v2/orders/test_meal.py
@@ -4,22 +4,88 @@ import json
 import pytest
 
 meal_data_cases = [
-    (dict(meal="Wali", category="BBQ Chicken", description="Saved with soup", price=500), 404),
-    (dict(category="BBQ Chicken", description="Saved with soup", price=500), 400),
-    (dict(meal="Wali",description="Saved with soup", price=500), 400),
-    (dict(meal="Wali", category="BBQ Chicken", price=500), 400),
-    (dict(meal="Wali", category="BBQ Chicken", description="Saved with soup"), 400),
-    (dict(meal=400, category="BBQ Chicken", description="Saved with soup", price=500), 400),
-    (dict(meal="Wali", category=40, description="Saved with soup", price=500), 400),
-    (dict(meal="Wali", category="BBQ Chicken", description="&%Saved with soup", price=500), 400),
-    (dict(meal="Wali", category="BBQ Chicken", description="Saved with soup", price=0), 400),
-    (dict(meal="Wali", category="BBQ Chicken", description="Saved with soup", price=500), 404),
+    (dict(
+        meal="Wali",
+        category="BBQ Chicken",
+        description="Saved with soup",
+        price=500), 404),
+    (dict(
+        category="BBQ Chicken",
+        description="Saved with soup",
+        price=500), 400),
+    (dict(
+        meal="Wali",
+        description="Saved with soup",
+        price=500), 400),
+    (dict(
+        meal="Wali",
+        category="BBQ Chicken",
+        price=500), 400),
+    (dict(
+        meal="Wali",
+        category="BBQ Chicken",
+        description="Saved with soup"), 400),
+    (dict(
+        meal=400,
+        category="BBQ Chicken",
+        description="Saved with soup",
+        price=500), 400),
+    (dict(
+        meal="Wali",
+        category=40,
+        description="Saved with soup",
+        price=500), 400),
+    (dict(
+        meal="Wali",
+        category="BBQ Chicken",
+        description="&%Saved with soup",
+        price=500), 400),
+    (dict(
+        meal="Wali",
+        category="BBQ Chicken",
+        description="Saved with soup",
+        price=0), 400),
+    (dict(
+        meal="Wali",
+        category="BBQ Chicken",
+        description="Saved with soup",
+        price=500), 404),
     (dict(), 400),
     ("", 400)
 ]
+
+
 @pytest.mark.parametrize("meal, status_code", meal_data_cases)
 def test_create_meal(test_client, meal, status_code):
     "Function that tests different cases of creating meal with some data"
-    response = test_client.post("/api/v2/menu", data=json.dumps(meal),
+    response = test_client.post(
+        "/api/v2/menu", data=json.dumps(meal),
         headers={"content-type": "application/json"})
     assert response.status_code == status_code
+
+
+def test_get_available_meals(test_client):
+    """Function that test availabily of meals"""
+    response = test_client.get("/api/v2/menu")
+    assert response.status_code == 404
+
+
+def test_get_unavailable_meals(test_client):
+    """Function that test unavailability of meals"""
+    response = test_client.get("/api/v2/menu")
+    assert response.status_code == 404
+
+
+@pytest.mark.parametrize(
+    "meal_id, status_code", [
+        (1, 404), ("a", 404)])
+def test_get_specific_menu(test_client, meal_id, status_code):
+    """Function that test requesting meal with deffent meal_ids"""
+    response = test_client.get("/api/v2/menu/" + str(meal_id))
+    assert response.status_code == status_code
+
+
+def test_get_meals_with_invalid_url(test_client):
+    """Function that test requecting meal without meal_id"""
+    response = test_client.get("/api/v2/menu/")
+    assert response.status_code == 404
