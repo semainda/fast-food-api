12d2360aedd89e8841d45784ee8840a80ccb8dc1
semainda <semaindas@gmail.com>
2018-10-02 20:27:52 +0300

    ft #160784430 Add create order method

--- app/api/v2/models/orders/order.py
+++ app/api/v2/models/orders/order.py
@@ -1,66 +1,29 @@
 """This module creates orders model and its operations"""
 # standard imports
 from datetime import datetime
+
 # local imports
-from app.db_setups import create_dev_db_tables
+from .order_status import status
+from ..models import BaseModel
 
 
-class OrdersModel:
+class Order(BaseModel):
     """This class methods for orders endpoints"""
-    def __init__(self, user_id=None, delivery_address=None):
-        self.created_date = datetime.now().strftime("%Y, %m, %d")
-        self.delivery_address = delivery_address
-        self.conn = create_dev_db_tables()
+    def __init__(self):
+        super().__init__()
+        self.created_date = str(datetime.now().strftime("%Y-%m-%d"))
 
-    def post_order(self):
+    def create_order(self, user_id, delivery_address, meal_id, quantity, description=None):
         """Method for create order"""
-        sql = """INSERT INTO orderss(
-            user_id,
-            delivery_address,
-            created_date) VALUES(%s, %s, %s);"""
-        cursor = self.conn.cursor()
-        cursor.execute(sql, (
-            self.user_id,
-            self.delivery_address,
-            self.created_date))
-        self.conn.commit()
-        self.conn.close()
+        sql = """WITH ordered AS(
+            INSERT INTO orders(user_id, delivery_address, description, created_date)
+            VALUES(%s, %s, %s, %s) RETURNING order_id)
+            INSERT INTO orders_items 
+            SELECT ordered.order_id, %s, %s FROM ordered RETURNING order_id;"""
+        self.cud_operations(sql, (user_id, delivery_address, description, self.created_date, meal_id, quantity))
 
-    def get_all_orders(self):
-        """Method for get all orders"""
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT * FROM orders;")
-        self.conn.close()
-        return cursor.fetchall()
+    
 
-    def get_order(self, order_id):
-        """Method for get a specific order"""
-        sql = """SELECT * FROM orders WHERE order_id=%s;"""
-        cursor = self.conn.cursor()
-        cursor.execute(sql, (order_id,))
-        self.conn.close()
-        return cursor.fetchone()
 
-    def put_order_delivery_address(self, order_id):
-        """Method for update specific order address"""
-        sql = "UPDATE orders SET delivery_address=(%s) WHERE order_id=(%s);"
-        cursor = self.conn.cursor()
-        cursor.execute(sql, (order_id,))
-        self.conn.commit()
-        self.conn.close()
 
-    def put_order_status(self, order_id):
-        """Method for update specific order status"""
-        sql = "UPDATE orders SET status=(%s) WHERE order_id=(%s);"
-        cursor = self.conn.cursor()
-        cursor.execute(sql, (order_id,))
-        self.conn.commit()
-        self.conn.close()
 
-    def delete_order(self, order_id):
-        """Method for delete specific order"""
-        sql = "DELETE FROM orders WHERE order_id=(%s);"
-        cursor = self.conn.cursor()
-        cursor.execute(sql, (order_id,))
-        self.commit()
-        self.close()
